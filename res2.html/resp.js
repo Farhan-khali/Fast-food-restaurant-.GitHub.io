008e21c5e61fe854584677a7edbb248db573287a
Farhan Khalid <118213864+Farhan-khali@users.noreply.github.com>
2023-01-21 13:42:54 +0500

    farhan khalid

---
+++ res2.html/resp.js
@@ -0,0 +1,6 @@
+burgar = document . querySelector('.burgar')
+navlist = document . querySelector('.navlist              ')
+
+burgar.addEventListener('click', ()=>{
+navlist.navlist.toggle('v-class');
+})
